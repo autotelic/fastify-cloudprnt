52ad7282a26181dfbab8feaedbf978b37e6274bb
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2023-01-11 15:31:42 -0800

    feat: add redis example

---
+++ examples/redis/index.js
@@ -0,0 +1,44 @@
+import { createClient } from 'redis'
+import view from '@fastify/view'
+import nunjucks from 'nunjucks'
+
+import fastifyCloudPrnt from '../../index.js'
+
+const JOB_DATA = 'printJobData'
+const PRINT_QUEUE = 'printJobQueue'
+const REDIS_URL = process.env.REDIS_URL
+
+export default async function basic (fastify, options) {
+  // redis connection url defaults to localhost on port 6379
+  const redis = createClient({ url: REDIS_URL })
+  await redis.connect()
+
+  await fastify.register(view, {
+    engine: { nunjucks },
+    root: './examples'
+  })
+
+  await fastify.register(fastifyCloudPrnt, {
+    queueJob: async (token, jobData) => {
+      await redis.HSET(JOB_DATA, token, JSON.stringify(jobData))
+      await redis.LPUSH(PRINT_QUEUE, token)
+    },
+    getJob: async () => {
+      const token = await redis.LINDEX(PRINT_QUEUE, 0)
+      return token === undefined ? null : token
+    },
+    getJobData: async token => {
+      const jsonJobData = await redis.HGET(JOB_DATA, token)
+      if (!jsonJobData) {
+        return null
+      }
+      return JSON.parse(jsonJobData)
+    },
+    deleteJob: async token => {
+      await redis.HDEL(JOB_DATA, token)
+      const deletedPrintJob = await redis.LREM(PRINT_QUEUE, 1, token)
+
+      return deletedPrintJob > 0
+    }
+  })
+}
